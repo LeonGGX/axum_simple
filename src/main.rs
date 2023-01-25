5d946f99c248b19fe70a79f3e771e96d08c4683c
LeonGGX <37880486+LeonGGX@users.noreply.github.com>
2023-01-25 11:32:30 +0100

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ use tower_http::trace::TraceLayer;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 pub mod askama;
+pub mod authentication;
 mod db;
 mod errors;
 mod globals;
@@ -26,6 +27,7 @@ mod sessions;
 
 use crate::db::connect::create_pg_pool;
 use crate::db::musicians::find_persons_by_name_parts;
+//use crate::db::genres::find_genre_by_name;
 use crate::handlers::axum_sessions_handlers::{get_session_hdl, set_session_hdl};
 use crate::handlers::genres_handlers::{
     create_genre_hdl, delete_genre_hdl, find_genre_by_name_hdl, list_genres_askama_hdl,
@@ -45,60 +47,69 @@ use crate::print_req_res::print_req_cookies_askama;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    //******************************************************
+    // initiating the tracing system
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| "axum_simple=debug,tower_http=info".into()),
+                .unwrap_or_else(|_| "axum_simple=debug,tower_http=debug".into()),
         )
-        .with(tracing_subscriber::fmt::layer())
+        .with(
+            tracing_subscriber::fmt::layer()
+                .with_target(false)
+                .without_time(),
+        )
         .init();
 
+    //*****************************************************
     // les données pour ouvrir la base de données Postgresql
+
+    // on va chercher les éléments pour ouvrir la DB dans les fichier .env
     dotenvy::dotenv().ok();
     let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
-
+    // on crée le pool Postgresql de sqlx
     let pool = create_pg_pool(&db_url).await?;
 
+    //*****************************************************
     // a layer with a redis session
     // uses async_redis_session and axum_session
     let session_layer = sessions::redis_session::create_redis_session_layer()
         .await
         .unwrap();
 
+    //******************************************************
     // axum-flash
     // vient de tower_cookies::Key
     let key = Key::generate();
     let flash_config = axum_flash::Config::new(key).use_secure_cookies(false);
 
+    //*******************************************************
     // Our application state
+    // It contains the DB pool and a configuration for flash cookies needed by axum_flash
     let state = AppState { pool, flash_config };
 
     //******************************************************
     // the Routers
     //
 
-    // # Router
     // A simple router to the about page
     // Attention the path must be "/" !!!
     let about_route = Router::new().route("/", get(about_hdl));
 
-    // # Router
     // A simple router to say hello
     let hello_routes = Router::new().route("/:name", get(hello_name_askama_hdl));
 
-    // # Router
-    // A simple router to debug app
+    // A router to debug app
     let debug_routes = Router::new()
         .route("/cookies", get(print_req_cookies_askama))
         .route("/set_session", get(set_session_hdl))
         .route("/get_session", get(get_session_hdl));
 
-    // # Router
+    // Authorisation Router
     // the route "/login" correspond to "/auth/login"
     let auth_routes =
         Router::new().route("/login", get(login_form_askama_hdl).post(post_login_hdl));
 
-    // # Router
     // A router with state : a PgPool is necessary and is to be found in the state
     // with_state(AppState{}) comes at the end or if it's general later in app
     // Handles all the routes to manage persons
@@ -107,6 +118,7 @@ async fn main() -> anyhow::Result<()> {
         .route("/", get(manage_persons_askama_hdl))
         .route("/add", post(create_person_hdl))
         .route("/delete/:id", post(delete_person_hdl))
+        .route("/:id", post(update_person_hdl))
         .route("/print", get(list_persons_askama_hdl))
         .route("/find", post(find_person_by_name_hdl));
 
@@ -162,9 +174,12 @@ async fn main() -> anyhow::Result<()> {
 }
 
 /// # Application State
-/// passed to the routers where needed with _router.with_state(state)_\
-/// contains a PgPool to connect to the DB\
-/// contains a flash_config needed by axum_flash
+/// passed to the routers where needed with _router.with_state(state)_
+/// and can be used by the handlers   
+///
+/// ## Fields
+/// - PgPool to connect to the DB
+/// - flash_config needed by axum_flash
 ///
 #[derive(Clone, FromRef)]
 pub struct AppState {
