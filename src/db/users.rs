87dec98eef92092306c799af0b33e91da238d9a7
LeonGGX <37880486+LeonGGX@users.noreply.github.com>
2023-04-28 18:29:39 +0200

    Add files via upload

--- src/db/users.rs
+++ src/db/users.rs
@@ -1,7 +1,12 @@
 //! src/db/users.rs
+//!
+//! CRUD operations on users (using struct User) with sqlx and PostgresQL DB
+//! Search operations on users in the DB
+//!
 
-//use crate::errors::MyAppError;
 use crate::models::user::{FilteredUser, NewUser, User};
+use chrono::{DateTime, Utc};
+use sqlx::postgres::types::PgTimeTz;
 use sqlx::PgPool;
 use uuid::Uuid;
 
@@ -29,12 +34,12 @@ pub async fn find_user_by_name(name: String, pool: &PgPool) -> sqlx::Result<User
 }
 
 ///
-/// find_user_by_credentials
+/// # find_user_by_credentials
 ///
-/// check the existence of a user based on the user name and password    
+/// Retrieves a user based on the user name and password fields in the DB    
 /// the password must be hashed before using the function.    
 ///
-/// returns a User or sqlxError
+/// returns a Result with User or sqlxError
 ///
 #[allow(dead_code)]
 pub async fn find_user_by_credentials(
@@ -63,7 +68,11 @@ pub async fn find_user_by_credentials(
     };
     Ok(user)
 }
-
+///
+/// # find_user_by_email
+/// Retrieves a user by the email field which is unique in the DB    
+/// Returns a Result with the corresponding user (User) or a sqlx::Error
+///
 #[allow(dead_code)]
 pub async fn find_user_by_email(email: String, pool: &PgPool) -> sqlx::Result<User> {
     let row = sqlx::query!("SELECT * FROM users WHERE email = $1", email)
@@ -83,7 +92,11 @@ pub async fn find_user_by_email(email: String, pool: &PgPool) -> sqlx::Result<Us
     };
     Ok(user)
 }
-
+///
+/// # find_user_by_id
+/// Retrieves a user by the id field which is unique in the DB    
+/// Returns a Result with the corresponding user (User) or a sqlx::Error
+///
 #[allow(dead_code)]
 pub async fn find_user_by_id(id: Uuid, pool: &PgPool) -> sqlx::Result<User> {
     let row = sqlx::query!("SELECT * FROM users WHERE id = $1", id)
@@ -110,14 +123,14 @@ CRUD FUNCTIONS
 ///
 /// Adds a user to the users table
 /// a hashed password must be set before entering the function
-///
+/// //RETURNING id, name, email, photo, verified, password, role",
 #[allow(dead_code)]
 pub async fn add_user(new_user: &NewUser, pool: &PgPool) -> sqlx::Result<User> {
     tracing::info!("fonction add_user: NewUser = {:#?}: ", new_user);
     let row = sqlx::query!(
         "INSERT INTO users (name, email, password, role)
-            VALUES ($1, $2, $3, $4)
-            RETURNING id, name, email, photo, verified, password, role",
+            VALUES ($1, $2, $3, $4)            
+            RETURNING *",
         new_user.name,
         new_user.email,
         new_user.password,
@@ -134,8 +147,8 @@ pub async fn add_user(new_user: &NewUser, pool: &PgPool) -> sqlx::Result<User> {
         verified: row.verified,
         password: row.password,
         role: row.role,
-        created_at: None,
-        updated_at: None,
+        created_at: row.created_at,
+        updated_at: row.updated_at,
     };
     tracing::info!("{:#?}", user);
     Ok(user)
@@ -144,7 +157,11 @@ pub async fn add_user(new_user: &NewUser, pool: &PgPool) -> sqlx::Result<User> {
 /*****************************************************************************
 DISPLAY FUNCTIONS
  */
-
+///
+/// # list_users
+/// Returns a Result with a list of users or sqlxError   
+/// The list is populated with FilteredUser to hide sensitive data
+///
 pub async fn list_users(pool: &PgPool) -> sqlx::Result<Vec<FilteredUser>> {
     let mut list_safe_users: Vec<FilteredUser> = Vec::new();
     let rows = sqlx::query!(
@@ -154,7 +171,7 @@ pub async fn list_users(pool: &PgPool) -> sqlx::Result<Vec<FilteredUser>> {
     .fetch_all(pool)
     .await?;
 
-    for mut row in rows {
+    for row in rows {
         list_safe_users.push(FilteredUser {
             id: row.id.to_string(),
             name: row.name,
@@ -162,8 +179,8 @@ pub async fn list_users(pool: &PgPool) -> sqlx::Result<Vec<FilteredUser>> {
             role: row.role,
             photo: row.photo,
             verified: row.verified,
-            created_at: row.created_at,
-            updated_at: row.updated_at,
+            created_at: row.created_at.unwrap(),
+            updated_at: row.updated_at.unwrap(),
         });
     }
     Ok(list_safe_users)
